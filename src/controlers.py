6e7e4811ca12c13fa0bde9cf42e25c3859fd2ded
Benjamin Church <benvoulgarischurch@gmail.com>
2017-08-05 17:18:47 -0400

    added ode integrator

--- src/controlers.py
+++ src/controlers.py
@@ -22,28 +22,51 @@
 #  
 #  
 
-from math import *
-from scipy.integrate import odeint 
-from scipy.special import expit 
+from math import * 
+import numpy as np
 
-max_rate = 1
+max_rate = 3
 
-def Pcontroler(x, y, theta, v, interror, params):
-	return get_error(x,y)*params[0];
+def sign(x):
+	if x >= 0:
+		return 1
+	else:
+		return -1
 
-def PIcontroler(x, y, theta, v, interror, params):
-	return get_error(x,y)*params[0] + det_derror(x, y, theta, v)*params[1];
-
-def get_error(x, y):
-	return dist((x, y))
+def PIDcontroler(x, y, theta, v, interror, params, line):
+	return np.dot(get_error(x, y, theta, v, interror, line), params)
 	
-def get_derror(x, y):
-	return diff_dist((x, y))
+def d_to_line(x, y, p0, p1):
+	return abs((p1[1] - p0[1])*x + (p1[0] - p0[0])*y - (p1[1] - p0[1])*p0[0] - (p1[0] - p0[0])*p0[1])/sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2)
 
-def car_rate_of_change_function(controller, params):
-	return lambda x, y, theta, v, interror : v*cos(theta), v*sin(theta), max_rate*expit(controller(x, y, interror, params)), 0, get_error(x, y) 
+def get_error(x, y, theta, v, interror, line):
+	i = np.argmin(list(map(lambda p : sqrt((x - p[0])**2 + (y - p[1])**2), line)))
+	
+	p = line[i]
+	
+	if i == 0:
+		p1 = line[i+1]
+		dist = d_to_line(x, y, p, p1)
+		
+	elif i == len(line) - 1:
+		p2 = line[i-1]
+		dist = d_to_line(x, y, p, p2)
+		
+	else:
+		p1, p2 = line[i-1], line[i+1]
+		dist = d_to_line(x, y, p1, p2)
+	
+	dist = sqrt((x - p[0])**2 + (y - p[1])**2)
+		
+	if dist == 0:
+		return [0, interror, v]
+	else:
+		return [sign((x - p[0])*sin(theta) - (y - p[1])*cos(theta))*dist, interror, (x - p[0])/dist * v*cos(theta) + (y - p[1])/dist * v*sin(theta)]
 	
 
+def car_rate_of_change_function(controller, params, line):
+	return lambda y, t : (y[3]*cos(y[2]), y[3]*sin(y[2]), max_rate*tanh(controller(y[0], y[1], y[2], y[3], y[4], params, line)), 0, get_error(y[0], y[1], y[2], y[3], y[4], line)[0])
+	
 
 def main(args):
     return 0
---
+++ src/integrator.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  controlers.py
+#  
+#  Copyright 2017 Benjamin Church <ben@U430>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+from math import *
+from scipy.integrate import odeint 
+from controlers import *
+import numpy as np
+from matplotlib import pyplot as plt
+
+def project(arr, i):
+	return list(map(lambda x: x[i], arr))
+
+def Return_Integration(controler, params, line, t_max, N):
+	y0 = [0, 0, 0, 1, 0]
+	t = np.linspace(0, t_max, N) 
+	sol = odeint(car_rate_of_change_function(controler, params, line), y0, t)
+	lables = ["x", "y", r"$\theta$", "v", r"$\int error$"]
+
+	for i in range(5):
+		if i != 3:
+			plt.plot(t, project(sol, i), label = lables[i])
+	
+	plt.plot(t, list(map(lambda x: get_error(x[0], x[1], x[2], x[3], x[4], line)[0], sol)), label = "error")
+	plt.legend()
+	
+	plt.figure()
+	plt.plot(project(line, 0), project(line, 1), label = "center line")
+	plt.plot(project(sol, 0), project(sol, 1), label = "path")
+	plt.legend()
+	plt.plot()
+	plt.show()
+	
+	return t, sol
+
+def center_line_func(x):
+	return (x, 1/(1 + exp(-(10*x-5))))
+
+def main(args):
+	N = 100
+	Time_Num = 1000
+	length = 1
+	line = list(map(center_line_func, np.linspace(0, length, N)))
+	Return_Integration(PIDcontroler, [100, 10, 5], line, length*2, Time_Num)
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
