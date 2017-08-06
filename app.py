a9bb5deb935ac7e87046433ec2cadf79e79c3521
Henry Williams <hwill12345@gmail.com>
2017-08-05 23:58:49 -0400

    render defaults

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route('/_example-0')
+def example0():
+	a = request.args.get('a', 10, type=int)
+	return mpld3.fig_to_html(run_example_0(a))
+
 @app.route('/_example-1')
 def example1():
-	a = request.args.get('a', 0, type=int)
+	a = request.args.get('a', 10, type=int)
 	return mpld3.fig_to_html(run_example_1(a))
+
+@app.route('/_example-2')
+def example2():
+	a = request.args.get('a', 10, type=int)
+	return mpld3.fig_to_html(run_example_2(a))
 
 if __name__ == "__main__":
 	app.run()
